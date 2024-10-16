67640649e7add815dbe7eac043af631881595c85
MASAOKA_HIDENOR\masan <playtest935@gmail.com>
2024-10-16 17:21:48 +0900

    モーダルウィンドウと時間取得を追加

--- src/pages/Timestamp_Page.jsx
+++ src/pages/Timestamp_Page.jsx
@@ -1,82 +1,48 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
 export const Timestamp_Page = () => {
+    
+    // 現在時刻を管理するuseStateを定義
+    const [currentTime, setCurrentTime] = useState();
 
-    const openDialog = () => {
-        const dialog = document.getElementById('myDialog');
-        dialog.showModal();
-    };
-
-    const closeDialog = () => {
-        const dialog = document.getElementById('myDialog');
-        dialog.close();
-    };
-
+    // useRefでの要素参照とダイヤログ制御関数
+    const dialogRef = useRef();
+    const openDialog = () => dialogRef.current.showModal();
+    const closeDialog = () => dialogRef.current.close();
     const dialogMessage = (title, message) => {
-        const dialog = document.getElementById("myDialog");
-        dialog.querySelector("h2").textContent = title;
-        dialog.querySelector("p").textContent = message;
+        
+        // 現在時刻を更新する関数を定義
+        const date = new Date;
+        const H = date.getHours(),
+                M = date.getMinutes(),
+                S = date.getSeconds();
+
+        setCurrentTime(`${H}:${M}:${S}`);
+
+        dialogRef.current.querySelector("h2").innerHTML = title;
+        dialogRef.current.querySelector("p").innerHTML = `${message}<br>${currentTime}`;
     };
+
 
     return (
         <div>
             <h2>タイムスタンプ</h2>
-            <button id="startWorkBtn" onClick={() =>{
-                dialogMessage("勤務開始","打刻完了");
+            <button id="startWorkBtn" onClick={() => {
+                dialogMessage("勤務開始", "打刻完了");
                 openDialog();
             }}>勤務開始</button>
 
-            <button id="startWorkBtn" onClick={() =>{
-                dialogMessage("勤務終了","打刻完了");
+            <button id="FinishWorkBtn" onClick={() => {
+                dialogMessage("勤務終了", "打刻完了");
                 openDialog();
-            }}>勤務完了</button>
+            }}>勤務終了</button>
 
-            <dialog id="myDialog">
-                <h2></h2>
-                <p></p>
+            {/* useRef参照用のref属性を追加 */}
+            <dialog ref={dialogRef}>
+                <h2>insert_dialogTitle!</h2>
+                <p>insert_dialogMassage!</p>
                 <button onClick={closeDialog}>閉じる</button>
             </dialog>
         </div>
     );
 };
-
-
-// import React from "react";
-
-// export const Timestamp_Page = () =>{
-
-//     const dialogStartWork = () => {
-//         TimeStamp_Button_Click({
-//             message : "勤務開始時刻が打刻されました",
-//             BtnId : "startWorkBtn"
-//         });
-//     };
-
-//     return (
-//         <>
-//             <h2>タイムスタンプ</h2>
-//             <button id="startWorkBtn" onclick={dialogStartWork}>勤務開始</button>
-//             <button id="finishWorkBtn">勤務終了</button>
-//         </>
-//     );
-// };
-
-// const TimeStamp_Button_Click = ({message, BtnId}) =>{
-    
-//     return (
-//         <div>
-//             <dialog id="timestampDialog">
-//                 <h2>{ message }</h2>
-//                 <button id="dialogCloseBtn">OK</button>
-//             </dialog>
-
-//             <script>
-//                 const dialog = document.getElementById("timestampDialog");
-//                 const openBtn = document.getElementById({ BtnId });
-//                 const closeBtn = document.getElementById("dialogCloseBtn");
-                
-//                 openBtn.addEventListener("click", () => { dialog.showModal(); } );
-//             </script>
-//         </div>
-//     );
-// };
