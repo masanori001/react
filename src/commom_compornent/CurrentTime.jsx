508620569e0cad8dbbe007b0e9a873a144102c2c
MASAOKA_HIDENOR\masan <playtest935@gmail.com>
2024-10-21 13:49:44 +0900

    リファクタ

--- src/commom_compornent/CurrentTime.jsx
+++ src/commom_compornent/CurrentTime.jsx
@@ -1,31 +1,34 @@
-import React, { useEffect,useRef,useState } from 'react';
+import React, { useEffect,useState } from 'react';
 
 export const CurrentTime = () => {
-
-    const timeString = useRef();
-
-    // 現在時刻を管理するuseStateを定義
-    const [currentTime, setCurrentTime] = useState(0);
-    // 現在時刻を取得し、更新する関数
-    const updateCurrentTIme = () =>{
+    
+    const fetchCurrentTime = () => {
         const date = new Date;
         const H = date.getHours(),
                 M = date.getMinutes(),
                 S = date.getSeconds();
-        setCurrentTime(`${H}:${M}:${S}`);
+        const CurrentTimeStrings = `${H}:${M}:${S}`;
+        return CurrentTimeStrings 
+    };
+
+    // 現在時刻を管理するuseStateを定義
+    const [currentTime, setCurrentTime] = useState(fetchCurrentTime());
+    // 現在時刻を取得し、更新する関数
+    const updateCurrentTIme = () =>{
+        setCurrentTime(fetchCurrentTime());
     };
 
     // 初回レンダリング時にのみ、実行(コンストラクタみたいなもん)
     useEffect(() => {
         // 1秒間隔で時刻更新関数を実行する
-        const intervalId = setInterval(updateCurrentTIme, 1000);
+        const intervalId = setInterval(updateCurrentTIme, );
         // クリーンナップ関数（このコンポーネントが削除される時に実行される)
         return () => { clearInterval(intervalId); };
     },[]);
 
     return (
         <>
-            <p id="timeString">{currentTime}</p>       
+            <p>{currentTime}</p>       
         </>
     );
 };
