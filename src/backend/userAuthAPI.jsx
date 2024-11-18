9ef8ea99d0d46cc2c07a8097339b10bab573f9be
MASAOKA_HIDENOR\masan <playtest935@gmail.com>
2024-11-18 23:32:05 +0900

    API作成

--- src/backend/userAuthAPI.jsx
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.get("/hello",(request,response) => {
-    response.send("hello world\n");
-});
-
-app.listen(3000);
--- src/pages/LoginForm.jsx
+++ src/pages/LoginForm.jsx
@@ -6,17 +6,16 @@
 // [4]メールアドレスでのパスワードチェンジ
 // [5]新規アカウント作成画面への遷移
 // [6]パスワードを●●●で隠す奴
-import React, { useState, useRef } from "react";
+import React, { useState, useRef } from 'react';
 
 
 export const LoginPage =()=> {
-
-    //入力フォームのバリデーションチェックテキスト
-    const [usernameText, setUsername] = useState("ユーザー名を入力してください");
-    const [passwordText, setPassword] = useState("パスワードを入力してください");
-
+    
     const submitAuthdata = useRef();
 
+    //入力フォームのバリデーションチェックテキスト
+    const [usernameText, setUsername] = useState('ユーザー名を入力してください');
+    const [passwordText, setPassword] = useState('パスワードを入力してください');
 
     // =========バリデーションチェック関数=========
     const judgeUsernameValid = (Username) => {
@@ -25,10 +24,10 @@ export const LoginPage =()=> {
 
         // 文字数が8以上で数字が3つ以上含まれている
         if (8 <= Username.length && Username.match(regexp)) {
-            setUsername("入力されたユーザー名はポリシーを満たしています。");
+            setUsername('入力されたユーザー名はポリシーを満たしています。');
         }
         else {
-            setUsername("入力されたユーザー名はポリシーを満たしていません");
+            setUsername('入力されたユーザー名はポリシーを満たしていません');
         }
         // デバック用
         console.log(Username.length);
@@ -43,10 +42,10 @@ export const LoginPage =()=> {
 
         // 文字数が8以上で数字が3つ以上含まれている
         if (8 <= Password.length && Password.match(regexp)) {
-            setPassword("入力されたパスワードはポリシーを満たしています。");
+            setPassword('入力されたパスワードはポリシーを満たしています。');
         }
         else {
-            setPassword("入力されたパスワードはポリシーを満たしていません");
+            setPassword('入力されたパスワードはポリシーを満たしていません');
         }
     };
         
@@ -55,44 +54,53 @@ export const LoginPage =()=> {
         event.preventDefault(); //ページリロード防止
 
         //バリデーションチェックの可否判定
-        if (usernameText=="入力されたユーザー名はポリシーを満たしています。" 
-            && passwordText=="入力されたパスワードはポリシーを満たしています。")
+        if (usernameText=='入力されたユーザー名はポリシーを満たしています。' 
+            && passwordText=='入力されたパスワードはポリシーを満たしています。')
             {   
-                console.log("フォームの検証成功");
+                console.log('フォームの検証成功');
                 // 入力フォームの状態を受け取り
-                const username = await submitAuthdata.current.querySelector("input");
-                const password = await submitAuthdata.current.querySelector("input");
-                // json形式の文字列生成
-                const jsonString = await "{'username':${username},'password':${password}}"
-                // json.stringify⇒文字列をjsonオブジェクトに変換
+                const username = await submitAuthdata.current.querySelector('input');
+                console.log(username);
+                const password = await submitAuthdata.current.querySelector('input');
+                console.log(password);
+                
+                // json形式のdictを作成
+                const jsonString = await {'username': username.value,'password': password.value}
+                console.log(jsonString);
+                // json.stringify ⇒ dictをjsonオブジェクトに変換
                 const userAuthJson = await JSON.stringify(jsonString);
+                console.log("userAuthJson");
+                console.log(userAuthJson);
 
                 // user認証のAPI
-                const url = await "http://localhost:3000/userAuth";
+                const url = await 'http://localhost:3001/userauth';
                 // ★例外処理は後で書く
                 const response = await fetch(url,{
-                    method: "POST",
+                    method: 'POST',
                     headers: {
-                        "Content-Type": "application/json",
+                        'Origin': 'http://localhost:3000',
+                        'Content-Type': 'application/json',
                     },
-                    body: JSON.stringify(userAuthJson)
+                    body: userAuthJson
                 })
                 const responseJson = await response.json;
+                console.log(response);
                 console.log(responseJson);
-
-
-                return "return true";
+                
+                // ページを遷移する処理
+                return 'return true';
 
             }else{
-                console.log("フォームの検証失敗");
-                return "return false";
+
+                console.log('フォームの検証失敗');
+                return 'return false';
             }
     };
 
     return(
-        <div className="loginForm">
+        <div className='loginForm'>
             <head>
-                <meta charset="UTF-8" />
+                <meta charset='UTF-8' />
                 <title>sample.html</title>
             </head>
 
@@ -101,21 +109,20 @@ export const LoginPage =()=> {
                     <h1>勤怠管理アプリケーション</h1>
                 </header>
 
-                <form method="POST" action="" onSubmit={validationCheckText} ref={submitAuthdata}>
+                <form action='' onSubmit={validationCheckText} ref={submitAuthdata}>
                     <div>
-                        Username : <input type="text" name="name" placeholder="username" autoComplete="username"
+                        Username : <input type='text' name='name' placeholder='username' autoComplete='username'
                                         onChange={(event) => judgeUsernameValid(event.target.value)} 
                                 />
                         <p>{ usernameText }</p>
                     </div>
                     <div>
-                        Password : <input type="password" name="pass" placeholder="password" autoComplete="current-password"
+                        Password : <input type='password' name='pass' placeholder='password' autoComplete='current-password'
                                     onChange={(event)=>judgePasswordValid(event.target.value)}
                                 />
                         <p>{passwordText}</p>
                     </div>
-                    {/* ユーザー名＆パスワードをバックエンドに送信 */}
-                    <input type="submit" value="送信" />
+                    <input type='submit' value='送信' />
                 </form>
             </body>
         </div>
